bd42669e139a8296411629bfda4a4f77d6f38496
KerryKss <kerryalejandroh@gmail.com>
2025-10-02 16:00:25 -0500

    Configuracion del servidor web en express

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const port = 8000; // puerto de escucha
 
 // Inicializacion del servidor y primera ruta
 app.get("/", (req, res) => {
-  res.send("Hola mi server en Express");
+    res.send("Hola mi server en Express");
 });
 
 // Inicio del servidor
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
